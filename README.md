8cba8bd3888f657465f81268be5c6528321cd651
Floyda <floyda@163.com>
2018-05-18 17:48:19 +0800

    Update 2018-05-18 17:48:19

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#code-coin-collector
+# Fake Commit
---
+++ fake-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+year=$(date "+%Y")
+date=$(date "+%Y-%m-%d")
+time=$(date "+%T")
+file_name=$year-collector.md
+
+msg="$date $time"
+echo $msg >> $file_name
+echo $msg > current-msg.md
+
+echo -e "Update $msg" > current-msg.md
+msg="cat current-msg.md"
+git add .
+git commit -m "$($msg)"
+git push
+
+if [ $(uname) == "Darwin" ]
+then
+   say "Git is OK"
+fi
