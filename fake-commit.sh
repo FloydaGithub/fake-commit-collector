490a58bc6eed9f8fb892615f252246512adc441f
floyda <floyda@163.com>
2018-05-18 18:30:25 +0800

    Update 2018-05-18 18:30:25

--- fake-commit.sh
+++ fake-commit.sh
@@ -5,7 +5,7 @@ date=$(date "+%Y-%m-%d")
 time=$(date "+%T")
 file_name=$year-collector.md
 
-msg="$date $time\n"
+msg="$date $time"
 echo $msg >> $file_name
 echo $msg > current-msg.md
 
@@ -13,7 +13,8 @@ echo -e "Update $msg" > current-msg.md
 msg="cat current-msg.md"
 git add .
 git commit -m "$($msg)"
-git push
+git push github master
+git push coding master
 
 if [ $(uname) == "Darwin" ]
 then
